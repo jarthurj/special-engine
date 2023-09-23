b022bf27fddb15c0bf7ed1cdbb9e3f46f2288a25
jarthurj <jaoldaker1@gmail.com>
2023-09-22 22:26:12 -0400

    NEED TO FINISH ADDING ERRORS THAT COULD HAPPEN IF NOT ALL PARTS OF A QUERY ARE FILLED OUT

--- new_magic/apps/card_search/views.py
+++ new_magic/apps/card_search/views.py
@@ -3,7 +3,7 @@ from .forms import CardSearch
 from .models import *
 from django.db.models.query import QuerySet
 from .query_functions import *
-
+from django.contrib import messages
 
 def index(request):
 	context = {
@@ -14,56 +14,74 @@ def index(request):
 def search(request):
 	for key, value in request.POST.items():
 		print(key, value, type(value))
-	all_cards = Card.objects.all()
+	errors = {}
 
-	name_cards = name_query(request.POST['card_name'])
+	if request.POST.getlist('colors') != [] and request.POST['exactly_or_not']:
+		errors['color_error'] = "Please select how to search colors!!!!!!"
+		for key, value in errors.items():
+			messages.error(request, value)
+		return redirect('index')
+	else:
+		all_cards = Card.objects.all()
 
-	color_cards = color_query(request.POST.getlist('colors'),
-		request.POST['exactly_or_not'])
+		name_cards = name_query(request.POST['card_name'])
 
-	color_identity_cards = color_identity_query(request.POST.getlist('colors_identity'))
+		color_cards = Card.objects.none()
 
-	rarity_cards = rarity_query(request.POST.getlist('rarity'))
+		color_cards = color_query(request.POST.getlist('colors'),
+			request.POST['exactly_or_not'])
 
-	mpt_cards = mpt_query(request.POST['mpt'],
-							request.POST['mpt_condition'],
-							request.POST['mpt_parameter'])
+		color_identity_cards = color_identity_query(request.POST.getlist('colors_identity'))
 
-	lrb_cards = lrb_query(request.POST['lrb'],request.POST['game_types'])
-	all_text_cards = all_text_query(request.POST['any_text'])
-	type_cards = type_query(request.POST['type_line'])
+		rarity_cards = rarity_query(request.POST.getlist('rarity'))
+		#NEED TO ADD IN ERRORS IF "-" IS SELECTED 
 
-	all_queries = [name_cards, color_cards,color_identity_cards,
-						rarity_cards, mpt_cards, lrb_cards, 
-						all_text_cards, type_cards]
+		mpt_cards = mpt_query(request.POST['mpt'],
+								request.POST['mpt_condition'],
+								request.POST['mpt_parameter'])
 
-	matching_queries = []
+		lrb_cards = lrb_query(request.POST['lrb'],request.POST['game_types'])
+		all_text_cards = all_text_query(request.POST['any_text'])
+		type_cards = type_query(request.POST['type_line'])
 
-	for query in all_queries:
-		if query != Card.objects.none():
-			matching_queries.append(query)
+		all_queries = [name_cards, color_cards, color_identity_cards,
+							rarity_cards, mpt_cards, lrb_cards, 
+							all_text_cards, type_cards]
 
-	matching_cards = matching_queries[0].intersection(*matching_queries)
-	matching_cards_ids = []
-	for x in matching_cards:
-		matching_cards_ids.append(x.id)
-	request.session['card_ids'] = matching_cards_ids
-	# context = {
-	# 		'cards':matching_cards,
-	# 	}
-	# return render(request, 'card_search/cards.html', context)
-	return redirect('cards',page=1)
+		matching_queries = []
+
+		for query in all_queries:
+			if query != Card.objects.none():
+				matching_queries.append(query)
+
+		matching_cards = matching_queries[0].intersection(*matching_queries)
+		matching_cards_ids = []
+		for x in matching_cards:
+			matching_cards_ids.append(x.id)
+		request.session['card_ids'] = matching_cards_ids
+		# context = {
+		# 		'cards':matching_cards,
+		# 	}
+		# return render(request, 'card_search/cards.html', context)
+		return redirect('cards',page=1)
 
 def card_pages(request,page):
-	if page ==1:
+	last_page = len(request.session['card_ids'])//60
+	if page == 1:
 		cards = request.session['card_ids'][:60]
-	else:
+	elif page != last_page:
 		cards = request.session['card_ids'][60*page:(60*page)+60]
+	elif page >=last_page:
+		page = last_page
+		cards = request.session['card_ids'][(60*(last_page-1))+60:]
 	to_union = []
 	for x in cards:
 		to_union.append(Card.objects.filter(id=x))
-	page_cards = to_union[0].union(*to_union)
+	page_cards = Card.objects.none().union(*to_union)
 	page_one = False
+	next_page = page+1
+	if page+1>=last_page:
+		next_page = last_page
 	if page == 1:
 		page_one = True
 	context = {
@@ -72,6 +90,7 @@ def card_pages(request,page):
 		'page_one':page_one,
 		'prev_page':page-1,
 		'next_page':page+1,
+		'last_page':last_page==page,
 	}
 	return render(request, 'card_search/cards.html', context)
 

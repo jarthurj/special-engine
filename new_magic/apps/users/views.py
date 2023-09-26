1a2424f135d277f60f692e21753b0410fe348b08
jarthurj <jaoldaker1@gmail.com>
2023-09-25 23:36:09 -0400

    login

--- new_magic/apps/users/views.py
+++ new_magic/apps/users/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render, HttpResponse, redirect
 from django.contrib.auth.models import User
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from .forms import UserRegistrationForm
+
+
 def index(request):
 	pass
 
@@ -16,19 +18,26 @@ def create_user(request):
 		print(key, value)
 	errors = {}
 	if request.POST['password2'] != request.POST['password1']:
-		errors['password_mismatch'] = "Passwords do not match! "
+		errors['password_mismatch'] = "Passwords do not match!"
 		for key, value in errors.items():
 				messages.error(request, value)
 		return redirect('register')
 	else:
 		User.objects.create_user(request.POST['email'], 
 								request.POST['username'],
-								request.POST['password1'],
-								)
+								request.POST['password1'],)
 		return redirect('/')
 
 def login(request):
-	context = {
-		'form':AuthenticationForm()
-	}
-	return render(request, 'users/login.html', context)
+	if request.method == 'POST':
+		username = request.POST['username']
+		password = request.POST['password']
+		user = authenticate(request, username=username, password=password)
+		return redirect(request,'/')
+	else:
+		context = {
+			'form':AuthenticationForm()
+		}
+		return render(request, 'users/login.html', context)
+
+def login_user(re)

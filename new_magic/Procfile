ccf613473f83dad0e2737bafc65c749c130dca81
jarthurj <jaoldaker1@gmail.com>
2023-10-03 15:13:03 -0400

    asdf

---
+++ new_magic/Procfile
@@ -0,0 +1,1 @@
+web gunicorn new_magic.wsgi:application --log-file -
--- new_magic/new_magic/settings.py
+++ new_magic/new_magic/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 from pathlib import Path
 from dotenv import load_dotenv
 import os
+import django_heroku
+import dj_database_url
 load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -128,3 +130,11 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+
+django_heroku.settings(locals())
+
+
+
